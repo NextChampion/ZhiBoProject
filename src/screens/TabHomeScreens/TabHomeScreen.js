d244798326693f1da937ede753d5de5345a08cf7
zhangcx <zhangcx01@mail.jj.cn>
2019-03-24 23:05:06 +0800

    整理代码格式

--- src/screens/TabHomeScreens/TabHomeScreen.js
+++ src/screens/TabHomeScreens/TabHomeScreen.js
@@ -7,55 +7,58 @@ import CXScrollableTabView from '../../components/CXScrollableTabView';
 import UI from '../../UI';
 
 export default class Splash extends Component {
-  static propTypes = {
-    navigation: PropTypes.shape({
-      navigate: PropTypes.func.isRequired,
-    }).isRequired,
-  };
+    static propTypes = {
+        navigation: PropTypes.shape({
+            navigate: PropTypes.func.isRequired,
+        }).isRequired,
+    };
 
-  state = {
-      list: [
-          {
-              title: '推荐',
-              id: 1,
-          },
-          {
-            title: 'LOL',
-            id: 2,
-        },
-        {
-            title: 'Dota2',
-            id: 3,
-        },
-        {
-            title: 'DNF',
-            id: 4,
-        },
-        {
-            title: '绝地求生',
-            id: 5,
-        },
-        {
-            title: '刺激战场',
-            id: 6,
-        },
-        {
-            title: '王者荣耀',
-            id: 7,
-        },
-      ]
-  }
+    state = {
+        list: [
+            {
+                title: '推荐',
+                id: 1,
+            },
+            {
+                title: 'LOL',
+                id: 2,
+            },
+            {
+                title: 'Dota2',
+                id: 3,
+            },
+            {
+                title: 'DNF',
+                id: 4,
+            },
+            {
+                title: '绝地求生',
+                id: 5,
+            },
+            {
+                title: '刺激战场',
+                id: 6,
+            },
+            {
+                title: '王者荣耀',
+                id: 7,
+            },
+        ]
+    }
 
-  componentDidMount() {}
+    componentDidMount() { }
 
-  render() {
-    return (
-      <Container style={{ paddingBottom: UI.IS_IPHONE_X ? 24 : 0 }}>
-        <CXScrollableTabView data={this.state.list} />
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container style={{ paddingBottom: UI.IS_IPHONE_X ? 24 : 0 }}>
+                <CXScrollableTabView data={this.state.list} tabItemStyle={styles.tabItemStyle} />
+            </Container>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
+    tabItemStyle: {
+        backgroundColor: UI.color.primary1,
+    }
 });
